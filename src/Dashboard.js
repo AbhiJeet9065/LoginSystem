b1013cca863dbf804f87df13e141914f0dc9eedc
Your Name <you@example.com>
2021-04-03 22:27:38 +0530

    added all code

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,64 +1,14 @@
-import logo from './logo.svg';
-import React, { Component } from "react";
-import './App.css';
-import { Drawer, Button, Radio, Space } from 'antd';
+import React from 'react';
+import './Dashboard.css';
+import TodoList from './Components/TodoList';
 
-
-export default class Dashboard extends Component {
   
-    constructor(props) {
-      super(props);
-      this.state = { visible: false, placement: 'left' };
-    }
-
-
-  showDrawer = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-
-  onClose = () => {
-    this.setState({
-      visible: false,
-    });
-  };
-
-  onChange = e => {
-    this.setState({
-      placement: e.target.value,
-    });
-  };
-  
-  render() {
-    const { placement, visible } = this.state;
+  function Dashboard() {
     return (
-      <>
-        <Space>
-          <Radio.Group defaultValue={placement} onChange={this.onChange}>
-            <Radio value="top">top</Radio>
-            <Radio value="right">right</Radio>
-            <Radio value="bottom">bottom</Radio>
-            <Radio value="left">left</Radio>
-          </Radio.Group>
-          <Button type="primary" onClick={this.showDrawer}>
-            Open
-          </Button>
-        </Space>
-        <Drawer
-          title="Basic Drawer"
-          placement={placement}
-          closable={false}
-          onClose={this.onClose}
-          visible={visible}
-          key={placement}
-        >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-        </Drawer>
-      </>
+      <div className="todo-app">
+        <TodoList />
+      </div>
     );
   }
-}
-  
+
+export default Dashboard;
